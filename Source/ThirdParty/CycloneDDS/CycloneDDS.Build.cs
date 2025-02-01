8ed9848f6b69370bd190f9911899a9567dda97f1
Nicolas Brugie <nicolasbrugie@gmail.com>
2025-02-01 22:31:26 +0100

    Cleanup & Add Linux compilation support

--- Source/ThirdParty/CycloneDDS/CycloneDDS.Build.cs
+++ Source/ThirdParty/CycloneDDS/CycloneDDS.Build.cs
@@ -11,42 +11,50 @@ public class CycloneDDS : ModuleRules
 
 		if (Target.Platform == UnrealTargetPlatform.Win64)
 		{
+			ConfigureWindows();
+		}
+		else if (Target.Platform == UnrealTargetPlatform.Linux)
+		{
+			ConfigureLinux();
+		}
+	}
+	
+	private void ConfigureWindows()
+	{
+		string IncludePath = Path.Combine(ModuleDirectory, "Windows", "Include");
+		string DllPath = Path.Combine(ModuleDirectory, "Windows", "Bin", "ddsc.dll");
+		string LibPath = Path.Combine(ModuleDirectory, "Windows", "Lib", "ddsc.lib");
+		string TargetOutputDll = Path.Combine("$(Binaries)", Path.GetFileName(DllPath));
+		
+		PublicIncludePaths.Add(IncludePath);
+		PublicAdditionalLibraries.Add(LibPath);
+		RuntimeDependencies.Add(TargetOutputDll, DllPath);
+        
+		PublicDefinitions.Add("__MINGW32__=1");
+		PublicDefinitions.Add("DDSRT_WITH_FREERTOS=0");
+		PublicDefinitions.Add("__GNUC__=1");
+		PublicDefinitions.Add("__clang__=1");
+		PublicDefinitions.Add("__GNUC_MINOR__=3");
+		PublicDefinitions.Add("__GNUC_PATCHLEVEL__=0");
+		PublicDefinitions.Add("__clang_major__=12");
+		PublicDefinitions.Add("__clang_minor__=0");
+		PublicDefinitions.Add("__clang_patchlevel__=0");
+	}
 
-            PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "includes"));
-
-			var DllPath = Path.Combine(ModuleDirectory, "x64", "Release", "ddsc.dll");
-			var LibPath = Path.Combine(ModuleDirectory, "x64", "Release", "ddsc.lib");
-			
-            // Add the import library
-            PublicAdditionalLibraries.Add(LibPath);
-
-			// Delay-load the DLL, so we can load it from the right place first
-
-            var TargetOutputDll = Path.Combine("$(ProjectDir)\\Binaries\\ThirdParty\\", Path.GetFileName(DllPath));
-
-
-            // Ensure that the DLL is staged along with the executable
-            RuntimeDependencies.Add(TargetOutputDll, DllPath);
-
-            PublicDelayLoadDLLs.Add(DllPath);
-
-
-            PublicDefinitions.Add("__MINGW32__=1");
-
-            PublicDefinitions.Add("DDSRT_WITH_FREERTOS=0");
-
-            //// Définition des macros pour le compilateur GCC
-            PublicDefinitions.Add("__GNUC__=1");
-
-            //// Définition des macros pour le compilateur Clang
-            PublicDefinitions.Add("__clang__=1");
-
-            //// Valeurs cohérentes pour les macros GCC/Clang
-            PublicDefinitions.Add("__GNUC_MINOR__=3");
-            PublicDefinitions.Add("__GNUC_PATCHLEVEL__=0");
-            PublicDefinitions.Add("__clang_major__=12");
-            PublicDefinitions.Add("__clang_minor__=0");
-            PublicDefinitions.Add("__clang_patchlevel__=0");
-        }
+	private void ConfigureLinux()
+	{
+		string IncludePath = Path.Combine(ModuleDirectory, "Linux", "Include");
+		string LibPath = Path.Combine(ModuleDirectory, "Linux", "Lib", "libddsc.so");
+        
+		PublicIncludePaths.Add(IncludePath);
+		PublicAdditionalLibraries.Add(LibPath);
+        
+		PublicDefinitions.Add("_WIN32=0");
+		PublicDefinitions.Add("__MINGW32__=0");
+		PublicDefinitions.Add("__VXWORKS__=0");
+		PublicDefinitions.Add("DDSRT_WITH_FREERTOS=0");
+		PublicDefinitions.Add("__STDC_VERSION__=201710L");
+		PublicDefinitions.Add("__clang__=1");
+		PublicDefinitions.Add("__clang_minor__=0");
 	}
 }
